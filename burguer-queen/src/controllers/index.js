d79c5b7f3422633ce7a533a8a3eebe88844aa986
danleonca <danleon022688@gmail.com>
2020-07-27 10:07:53 -0500

    taking out the console.log from the firebase functions

--- burguer-queen/src/controllers/index.js
+++ burguer-queen/src/controllers/index.js
@@ -6,7 +6,6 @@ const db = firebase.firestore();
 export const createTable = async (element) => {
   let dates = moment(new Date());
   let dateIni = dates.hour() * 60 + dates.minute();
-  console.log("element", element);
   const newTable = {
     ...element,
     status: "Enviado a cocina",
@@ -14,7 +13,6 @@ export const createTable = async (element) => {
     timePrep: dateIni,
     timeFinal: "",
   };
-  console.log(newTable);
   return db.collection("tables").add(newTable);
 };
 
@@ -35,7 +33,6 @@ export const showTables2 = (cb) => {
       const result = [];
       querySnapshot.forEach(function (doc) {
         let arrayData = { id: doc.id, ...doc.data() };
-        // console.log (arrayData)
         result.push(arrayData);
       });
 
@@ -44,7 +41,6 @@ export const showTables2 = (cb) => {
 };
 
 export const showInfoTables2 = (cb, id) => {
-  //      const db = firebase.firestore()
   return db
     .collection("tables")
     .doc(id)
@@ -55,7 +51,6 @@ export const showInfoTables2 = (cb, id) => {
 };
 
 export const showInfoTables = async (id) => {
-  //const db = firebase.firestore()
   const docRef = db.collection("tables").doc(id);
   const info = await docRef.get();
   if (info.exists) {
@@ -68,8 +63,6 @@ export const showInfoTables = async (id) => {
 export const register = async (email, pass) => {
   const res = await firebase.auth().createUserWithEmailAndPassword(email, pass);
   if (res.user) {
-    console.log(res.user);
-
     await db.collection("usuarios").doc(res.user.uid).set({
       fechaCreacion: Date.now(),
       email: res.user.email,
@@ -84,17 +77,15 @@ export const register = async (email, pass) => {
 export const login = async (email, pass) => {
   const res = await firebase.auth().signInWithEmailAndPassword(email, pass);
   if (res.userSigned) {
-    console.log(res.userSigned);
     return res.userSigned;
   } else if (res.error) {
-    console.log(res.error);
+    return (res.error);
   }
 };
 
 export const userLog = () => {
   let logedUser = firebase.auth().currentUser;
   if (logedUser) {
-    console.log("existe");
     return logedUser;
   } else {
     console.log("no existe");
@@ -102,20 +93,17 @@ export const userLog = () => {
 };
 
 export const edit = async (id, tarea) => {
-  //const db = firebase.firestore()
   await db.collection("tables").doc(id).update({
     status: tarea,
   });
 };
 
 export const editTime = async (id, tarea) => {
-  //const db = firebase.firestore()
   await db.collection("tables").doc(id).update({
     timeOut: tarea,
   });
 };
 export const editTimeFinal = async (id, tarea) => {
-  //const db = firebase.firestore()
   await db.collection("tables").doc(id).update({
     timeFinal: tarea,
   });
@@ -132,7 +120,6 @@ export function createUser(emailNew, password) {
     .createUserWithEmailAndPassword(emailNew, password)
     .catch((error) => {
       const errorCode = error.code;
-      console.log(errorCode);
     });
 }
 
@@ -142,10 +129,8 @@ export const theWatcher = (state) => {
       console.log("active user");
       state(user);
       let userSigned = firebase.auth().currentUser;
-      console.log(userSigned);
       return userSigned;
     } else {
-      console.log("no users logged");
       state(null);
     }
   });
