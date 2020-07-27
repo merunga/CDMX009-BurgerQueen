89a42190c8313133bc89de7c44af22984791b804
shari12 <shariperez97@gmail.com>
2020-07-27 11:25:04 -0500

    Updates

---
+++ precache-manifest.2cccf9455c45ab2b54c8a5b0fcd78665.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdf9aa20d7e95078239feffbe04a8d5",
+    "url": "/shari12/CDMX009-BurgerQueen/index.html"
+  },
+  {
+    "revision": "8779e29767f33a0a3332",
+    "url": "/shari12/CDMX009-BurgerQueen/static/css/main.c5eebae7.chunk.css"
+  },
+  {
+    "revision": "da3eacc7ced804a8aabf",
+    "url": "/shari12/CDMX009-BurgerQueen/static/js/2.36d2bdd7.chunk.js"
+  },
+  {
+    "revision": "1099028804e06beaeac5e44e46fd8c78",
+    "url": "/shari12/CDMX009-BurgerQueen/static/js/2.36d2bdd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8779e29767f33a0a3332",
+    "url": "/shari12/CDMX009-BurgerQueen/static/js/main.2cab3ffd.chunk.js"
+  },
+  {
+    "revision": "806facf2d2aa49f9ea68",
+    "url": "/shari12/CDMX009-BurgerQueen/static/js/runtime-main.b4969754.js"
+  },
+  {
+    "revision": "0d1f8ed5e0705aa1c11d1d2f3972c896",
+    "url": "/shari12/CDMX009-BurgerQueen/static/media/burgerStatus.0d1f8ed5.svg"
+  },
+  {
+    "revision": "f5f7b6fc1459a2e58fbf87e8b278db2c",
+    "url": "/shari12/CDMX009-BurgerQueen/static/media/hamburger.f5f7b6fc.png"
+  },
+  {
+    "revision": "563840f58f3314c37204ba87f3f33574",
+    "url": "/shari12/CDMX009-BurgerQueen/static/media/kitchen.563840f5.svg"
+  },
+  {
+    "revision": "2cc136b6891dfb9ba6d6b9a6c8d4910f",
+    "url": "/shari12/CDMX009-BurgerQueen/static/media/queen.2cc136b6.svg"
+  },
+  {
+    "revision": "8a4f8da284e05f2f8145e2f1d3718e06",
+    "url": "/shari12/CDMX009-BurgerQueen/static/media/waiter.8a4f8da2.svg"
+  }
+]);
